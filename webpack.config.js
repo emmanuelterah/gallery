a8ffcc5be1e0fc9735ba2b26e594c6ee5b6f624f
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-09-23 19:14:14 +0300

    added webpack to script

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'emmanuel.js',
+    filename: 'blake.js',
   },
   target: 'node',
   externals: [nodeExternals()],
