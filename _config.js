1067f9d92e2b746fa7af325dd83a7aa777695dd1
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-09-23 10:04:33 +0300

    added MILESTONE 2 TITLE

--- _config.js
+++ _config.js
@@ -1,9 +1,17 @@
 var config = {}
 
 // Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
-module.exports = config;
+const config = {
+    mongoURI: {
+      production: 'mongodb+srv://emmanuelterah:terahterah98!@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+      development: 'mongodb+srv://emmanuelterah:terahterah98!@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+      test: 'mongodb+srv://emmanuelterah:terahterah98!@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    }
+  };
+  
+  module.exports = config;
+  
+
+
+
+  
